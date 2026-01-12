3b43a3b643df1d0ba1b17596a0ab99c0cfca23e2
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-12 20:08:28 +0530

    Update call.py

--- core/call.py
+++ core/call.py
@@ -1,4 +1,24 @@
 from pytgcalls import PyTgCalls
+from pytgcalls.types import Update
+from pytgcalls.types.stream import StreamAudioEnded
+
 from core.client import user
+from core.queues import pop, is_empty
+from plugins.play import play_next
 
 call_py = PyTgCalls(user)
+
+
+@call_py.on_stream_end()
+async def on_stream_end(_: PyTgCalls, update: Update):
+    chat_id = update.chat_id
+
+    pop(chat_id)
+
+    if not is_empty(chat_id):
+        await play_next(chat_id)
+    else:
+        try:
+            await call_py.leave_group_call(chat_id)
+        except Exception:
+            pass

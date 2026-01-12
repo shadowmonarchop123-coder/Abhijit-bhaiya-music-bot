8989fb143aea3fd4a473c8d2f1490362360aa295
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-12 20:57:41 +0530

    Update call.py

--- core/call.py
+++ core/call.py
@@ -4,26 +4,20 @@ from pytgcalls.types.stream import StreamAudioEnded
 
 from core.client import user
 from core.queues import pop, is_empty
-# Yahan se plugins.play hata diya gaya hai loop todne ke liye
+from plugins.play import play_next
 
 call_py = PyTgCalls(user)
 
 
 @call_py.on_stream_end()
 async def on_stream_end(_: PyTgCalls, update: Update):
-    # LOCAL IMPORT: Jab gaana khatam hoga, tabhi ye play_next ko bulayega
-    from plugins.play import play_next
-    
     chat_id = update.chat_id
 
-    # Queue se khatam hua gaana nikalein
     pop(chat_id)
 
     if not is_empty(chat_id):
-        # Agar queue mein aur gaane hain, to agla bajayein
         await play_next(chat_id)
     else:
-        # Agar queue khali hai, to VC chhod dein
         try:
             await call_py.leave_group_call(chat_id)
         except Exception:

0b704776d52af2a1d8e7ce4fe067d552158dd169
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-13 09:45:35 +0530

    Update call.py

--- core/call.py
+++ core/call.py
@@ -1,22 +1,18 @@
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
-from pytgcalls.types.stream import StreamAudioEnded
-
-from core.client import user
-from core.queues import pop, is_empty
+from core.client import user #
+from core.queues import pop, is_empty #
 
 call_py = PyTgCalls(user)
-
 
 @call_py.on_stream_end()
 async def on_stream_end(_: PyTgCalls, update: Update):
     chat_id = update.chat_id
+    pop(chat_id) #
 
-    pop(chat_id)
-
-    if not is_empty(chat_id):
-        # 🔁 yahan lazy import (circular fix)
-        from plugins.play import play_next
+    if not is_empty(chat_id): #
+        # Circular import se bachne ke liye core.streamer use karein
+        from core.streamer import play_next
         await play_next(chat_id)
     else:
         try:

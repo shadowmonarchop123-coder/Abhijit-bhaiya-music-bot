2f55d042eb4c05a295623a0b2cdfbda2a80dc100
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-14 09:39:23 +0530

    Update streamer.py

--- core/streamer.py
+++ core/streamer.py
@@ -1,21 +1,22 @@
 from pytgcalls.types.input_stream import InputStream, AudioPiped
 from pytgcalls import StreamType
 from core.call import call_py
-from core.queues import get, is_empty
+from core.queues import get, pop, is_empty
+from core.prefetch import get_prefetch
+
 
 async def start_stream(chat_id, url):
-    stream = InputStream(
-        AudioPiped(url)
-    )
-
     try:
         await call_py.join_group_call(
             chat_id,
-            stream,
+            InputStream(AudioPiped(url)),
             stream_type=StreamType().pulse_stream
         )
     except:
-        await call_py.change_stream(chat_id, stream)
+        await call_py.change_stream(
+            chat_id,
+            InputStream(AudioPiped(url))
+        )
 
 
 async def play_next(chat_id):
@@ -27,4 +28,9 @@ async def play_next(chat_id):
         return
 
     song = get(chat_id)
+
+    prefetched = get_prefetch(chat_id)
+    if prefetched:
+        song = prefetched
+
     await start_stream(chat_id, song["url"])

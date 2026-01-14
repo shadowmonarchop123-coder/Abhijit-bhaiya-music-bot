bff98c6729773cb5c04032e9af45ac04fc765722
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-14 13:24:03 +0530

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -5,21 +5,10 @@ from pyrogram.types import Message
 from core.client import app
 from core.streamer import play_next
 from core.queues import add, is_empty, clear
-from core.prefetch import prefetch
 
-from pytgcalls.exceptions import GroupCallNotFound, NoActiveGroupCall
-
-from youtubesearchpython import VideosSearch
-from core.prefetch import _extract as yt_stream
 from core.prefetch import extract_async, prefetch
 
-
-def yt_search(q):
-    search = VideosSearch(q, limit=1)
-    r = search.result()
-    if not r["result"]:
-        return None
-    return r["result"][0]["link"]
+from pytgcalls.exceptions import GroupCallNotFound, NoActiveGroupCall
 
 
 @app.on_message(filters.command(["play", "p"]) & filters.group)
@@ -33,6 +22,7 @@ async def play_cmd(_, message: Message):
     msg = await message.reply("⚡ Processing...")
 
     try:
+        # ⚡ TURBO extract (search + stream url in one hit)
         data = await extract_async(query)
     except Exception as e:
         return await msg.edit(f"❌ Stream error\n<code>{e}</code>")
@@ -45,7 +35,7 @@ async def play_cmd(_, message: Message):
     first = is_empty(chat_id)
     add(chat_id, song)
 
-    # 🔥 instant next preload
+    # 🔥 background prefetch (next instant)
     asyncio.create_task(prefetch(chat_id, query))
 
     if first:

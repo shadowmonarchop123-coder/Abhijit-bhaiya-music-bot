f0ff7122a9ec5d24c2a3e69c1ac01f47d1b231d5
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-14 09:34:15 +0530

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -5,10 +5,11 @@ from pyrogram.types import Message
 from core.client import app
 from core.streamer import play_next
 from core.queues import add, is_empty, clear
-from core.prefetch import turbo_extract
 
 from pytgcalls.exceptions import GroupCallNotFound, NoActiveGroupCall
+
 from youtubesearchpython import VideosSearch
+import yt_dlp
 
 
 def yt_search(q):
@@ -17,6 +18,28 @@ def yt_search(q):
     if not r["result"]:
         return None
     return r["result"][0]["link"]
+
+
+def yt_stream(url):
+    ydl_opts = {
+        "quiet": True,
+        "no_warnings": True,
+        "cookiefile": "cookies.txt",
+        "format": "bestaudio/best",
+        "skip_download": True,
+        "nocheckcertificate": True,
+        "geo_bypass": True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        if "entries" in info:
+            info = info["entries"][0]
+
+        return {
+            "title": info.get("title", "Unknown"),
+            "url": info["url"]
+        }
 
 
 @app.on_message(filters.command(["play", "p"]) & filters.group)
@@ -35,11 +58,11 @@ async def play_cmd(_, message: Message):
         if not query.startswith("http"):
             link = await loop.run_in_executor(None, yt_search, query)
             if not link:
-                return await msg.edit("❌ No results.")
+                return await msg.edit("❌ No result found")
         else:
             link = query
 
-        data = await loop.run_in_executor(None, turbo_extract, link)
+        data = await loop.run_in_executor(None, yt_stream, link)
 
     except Exception as e:
         return await msg.edit(f"❌ Stream error\n<code>{e}</code>")
@@ -58,6 +81,6 @@ async def play_cmd(_, message: Message):
             await msg.edit(f"▶️ <b>Now Playing:</b> {data['title']}")
         except (GroupCallNotFound, NoActiveGroupCall):
             clear(chat_id)
-            await msg.edit("❌ VC start karo + assistant add karo.")
+            await msg.edit("❌ VC start karo aur assistant add karo")
     else:
         await msg.edit(f"➕ <b>Queued:</b> {data['title']}")

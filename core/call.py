3d2c00d263d877234428bdc50c30d8a9d8d3d73b
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-01-14 09:26:36 +0530

    Update call.py

--- core/call.py
+++ core/call.py
@@ -1,21 +1,20 @@
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
-from core.client import user #
-from core.queues import pop, is_empty #
+from core.client import user
+from core.queues import pop, is_empty
 
 call_py = PyTgCalls(user)
 
 @call_py.on_stream_end()
 async def on_stream_end(_: PyTgCalls, update: Update):
     chat_id = update.chat_id
-    pop(chat_id) #
+    pop(chat_id)
 
-    if not is_empty(chat_id): #
-        # Circular import se bachne ke liye core.streamer use karein
+    if not is_empty(chat_id):
         from core.streamer import play_next
         await play_next(chat_id)
     else:
         try:
             await call_py.leave_group_call(chat_id)
-        except Exception:
+        except:
             pass
